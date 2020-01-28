6476585745d5dfa1cb1a57375152adc1713010ca
ARAB <ahmedmohamed23345@gmail.com>
2020-01-28 17:10:55 +0200

    ch

---
+++ app/src/main/java/com/technology/circles/apps/omanmade/models/SponsorsModel.java
@@ -0,0 +1,26 @@
+package com.technology.circles.apps.omanmade.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class SponsorsModel implements Serializable {
+    private List<Sponsors> sponsors;
+
+    public List<Sponsors> getSponsors() {
+        return sponsors;
+    }
+
+    public class Sponsors
+    {
+        private int id;
+            private String logo;
+
+        public int getId() {
+            return id;
+        }
+
+        public String getLogo() {
+            return logo;
+        }
+    }
+}
