6ce28716b6f065bb5da6e9d543f5c5aee7a89882
Emadmagdy151995Fci <emadmagdi.151995@gmai.com>
2020-01-28 10:58:30 +0200

    emad

--- app/src/main/java/com/technology/circles/apps/omanmade/tags/Tags.java
+++ app/src/main/java/com/technology/circles/apps/omanmade/tags/Tags.java
@@ -2,8 +2,9 @@ package com.technology.circles.apps.omanmade.tags;
 
 public class Tags {
 
-    public static String base_url = "http://firstharaj.com.sa/";
-    public static final String IMAGE_AVATAR = base_url+"storage/avatars/";
+    public static String base_url1 = "https://www.omanmade.com/wp-json/wp/v2";
+    public static String base_url2 = "http://madeinoman.build/api";
+
     public static final String session_login = "login";
     public static final String session_logout = "logout";
 
